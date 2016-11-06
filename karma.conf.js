7eeae45466ca665e4960f997b2f8529db4952cbc
Grzegorz Eliszewski <grzegorz@eliszewski.pl>
2016-11-06 20:59:06 +0100

    TRy to enable coverage

--- karma.conf.js
+++ karma.conf.js
@@ -3,16 +3,39 @@ module.exports = function (config) {
   config.set({
     browsers: ['PhantomJS'],
     frameworks: ['browserify', 'jasmine'],
-    files: ['test/unit/**/*.js'],
-    reporters: ['spec'],
+    files: [
+      'test/unit/**/*.js',
+      'node_modules/vue/dist/vue.js',
+      'node_modules/vue-resource/dist/vue-resource.js',
+      'app.js'
+    ],
+    reporters: ['spec', 'coverage'],
     preprocessors: {
-      'test/unit/**/*.js': ['browserify']
+      'test/unit/**/*.js': ['browserify', 'coverage']
+    },
+    vue: {
+      loaders: {
+        js: 'isparta'
+      }
     },
     browserify: {
       debug: true,
       // needed to enable mocks
       plugin: [require('proxyquireify').plugin]
     },
+    extensions: ['.vue', '.js'],
+    // transform: [
+    //     ['vueify'],
+    //     istanbul({
+    //         instrumenterConfig: { embedSource: true },
+    //         instrumenter: isparta,
+    //         ignore: [
+    //             '**/node_modules/**',
+    //             '**/test/**'
+    //         ]
+    //     }),
+    //     ['babelify']
+    // ]
     // if you want to continuously re-run tests on file-save,
     // replace the following line with `autoWatch: true`
     singleRun: true
